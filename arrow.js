eef9fb699b6c9a674281d9194841f48c8390553d
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-23 02:11:36 +0600

    Arrow Function, Multiple Parameter, Function Body

---
+++ arrow.js
@@ -0,0 +1,31 @@
+//30-4 Arrow Function, Multiple Parameter, Function Body
+
+// Function Declearation ***
+
+// function a(n, m) {
+  
+//     return n + m;
+
+// }
+
+// const res = a(49, 33);
+// console.log(res)
+
+
+
+// Function Expression ***
+
+//  const add = function(n, m) {
+  
+//     return n + m;
+
+//  }
+
+
+// Arrow Function ***
+ 
+// const arrowAdd = (n, m) => n + m;
+
+// const res = arrowAdd(49, 33);
+
+// console.log('Sum with Arrow Function ',res)
