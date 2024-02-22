f04f496b49605ef6d988f856722ced5c7e8690f0
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-23 02:22:24 +0600

    More arrow funtion is done

---
+++ more_arrow.js
@@ -0,0 +1,16 @@
+//30-5 More Arrow Functions And Big Arrow Function
+
+
+//Arrow funtion use only 3 way
+
+// Use No peramiter
+const array = () => console.log('Hello Pappu');
+array();
+
+// Single peramiter
+const sarray = a => console.log('Hello Pappu id is ' , a);
+sarray(50);
+
+// Multiple peramiter
+const marray = (a,b) => console.log('Hello Pappu id is ' , a ,'and', b);
+marray(1,2);
