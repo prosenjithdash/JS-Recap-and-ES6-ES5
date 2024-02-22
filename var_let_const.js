15f4d8c6688ac544b387f1167b4b06ea80690304
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-23 00:22:20 +0600

    Done var let and const use process

---
+++ var_let_const.js
@@ -0,0 +1,23 @@
+/*
+
+var: no any reason for use var
+let: allow it to reassign
+const: do not allow it to reassign 
+
+
+*/
+// var
+var n1 = 50;
+console.log(n1);
+
+// let
+let money = 60;
+// allow
+money = 40;
+console.log(money);
+
+// const
+const rich = 333;
+// not allow
+// rich = 70;
+console.log(rich);
